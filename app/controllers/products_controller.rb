a6907b366d4c78e2793439554863c29d1ce3a1ca
Rowan Cota <cota.rg@gmail.com>
2016-05-03 14:41:27 -0700

    working on products controller.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,25 @@
 class ProductsController < ApplicationController
   def index
+    # right now this generates a list of every product in the products table.
     @products = Product.order(id: :asc)
     render :index
   end 
       
-  def show 
+  def show
+    # this needs to be amended to hook into the session 
+    @user = User.find(params[:id])
+    redirect_to index
   end
 
   def new
+    @user = User.find(params[:id])
+    @product = Product.new
+    render :create_product
   end
 
   def create 
+    @product = Product.create(product_create_params[:product])
+    redirect_to products_show_path(@product.vendor_id)
   end
 
   def edit

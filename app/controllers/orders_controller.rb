d25fbdef1423293ffff03748817e193629cacbe1
Jade Vance <jadenicolevance@gmail.com>
2016-05-03 10:02:48 -0700

    Created the seven main actions for each controller.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+  def index
+  end 
+      
+  def show 
+  end
+
+  def new
+  end
+
+  def create 
+  end
+
+  def edit
+  end 
+
+  def update
+  end
+
+  def delete
+  end 
+
 end

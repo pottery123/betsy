85ff46059f908da1fa53f6ecc1575853b88e149d
Leah <leahnpetersen@gmail.com>
2016-05-03 14:29:38 -0700

    validated name, email and added has_secure_password to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   has_many :orders
   has_many :reviews , :through => :product
   has_many :order_items , :through => :order
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  has_secure_password
 end

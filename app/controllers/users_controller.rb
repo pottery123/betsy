e8f884bb5312f642decb972113d3462dd5cb9ed6
Leah <leahnpetersen@gmail.com>
2016-05-04 14:47:26 -0700

    changed name to user_name in the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_create_params[:user])
+    # raise
     if @user.save
       redirect_to root_path
     else
@@ -15,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_create_params
-    params.permit(user: [:user_name, :email_address, :password, :password_confirmation])
+    params.permit(user: [:user_name, :email, :password, :password_digest])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :reviews , :through => :product
   has_many :order_items , :through => :order
 
-  validates :name, presence: true
+  validates :user_name, presence: true
   validates :email, presence: true, uniqueness: true
 
   has_secure_password

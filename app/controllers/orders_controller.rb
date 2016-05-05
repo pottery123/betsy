dc74941ec43e200e2cb09fc4c7d17535835252ec
Jade Vance <jadenicolevance@gmail.com>
2016-05-04 20:27:27 -0700

    Shopping cart view works. Added a CSS shopping cart / fa fa-cart for added flare.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,53 @@
 class OrdersController < ApplicationController
-  def index
-  end 
-      
+ 
   def show 
+    # array them because they are relations. Can't .each a relation object 
+    @order_items = OrderItem.all.to_a
+    @product = Product.all .to_a
+    # @order_items = OrderItem.where(order: params[:user_id])
+    # @product = Product.find(user_id: params[:user_id])
+
   end
 
-  def new
-  end
+  # def new
+  #   # @product = Product.find(params[:product_id])
+  #   @product = Product.all 
+  #   @order = Order.new
 
-  def create 
-  end
+  # end
 
-  def edit
-  end 
+  # def create 
+  #   # @product = Product.find(params[:product_id])
+  #   @product = Product.all 
+  #   @order = Order.new
+  #   if @order.save
+  #     # flash message?
+  #     redirect_to orders_path
+  #   else
+  #     render :new
+  #   end 
+  # end
 
-  def update
-  end
+  # def edit
+  #   @order = Order.find(params[:id]) || Order.find(params[:order_id])
+  # end 
 
-  def delete
-  end 
+  # def update
+  #   @order = Order.find(params[:id]) 
+  #   # logic to check inventory 
+  #   # if quantity requested < quantity in stock, 
+  #   # message to the user "sorry we only have x of that item"
+  #   @order.update(update_params[:order])
+  #   # message about quantity updated 
+  #   redirect_to order_path
+  # end
+
+  # def destroy
+  #   Order.destroy(params[:id])
+  #   if params[:id] = true
+  #     # flash message
+  #     redirect_to order_path
+  #   end
+  # end 
 
 end

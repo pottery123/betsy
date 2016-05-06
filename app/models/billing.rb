1767e3aead2a1fc1a8187975e0fc6d419163c5c5
Jade Vance <jadenicolevance@gmail.com>
2016-05-05 23:14:41 -0700

    email validations working now.

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,6 +1,6 @@
 class Billing < ActiveRecord::Base
   belongs_to :order 
-  validates :email, presence: true, uniqueness: true, length: { in: 1..30 }
+  validates :email, presence: true, format: {with: /@/}
   validates :address, presence: true, uniqueness: true, length: { in: 1..50 }
   validates :name_on_cc, presence: true, length: { in: 1..50 }
   validates :security_on_cc, presence: true, numericality: { only_integer: true }, length: { in: 3 }

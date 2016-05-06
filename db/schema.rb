4ca61b32793d68a28a852ef4b0a385a3dc7e3d83
Rowan Cota <cota.rg@gmail.com>
2016-05-06 11:24:23 -0700

    wrote migrations to fix wrong column, add to right table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506175814) do
+ActiveRecord::Schema.define(version: 20160506182233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,10 +29,9 @@ ActiveRecord::Schema.define(version: 20160506175814) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text     "name"
-    t.text     "description", null: false
   end
 
   create_table "order_items", force: :cascade do |t|
@@ -60,6 +59,7 @@ ActiveRecord::Schema.define(version: 20160506175814) do
     t.integer  "quantity"
     t.boolean  "visible"
     t.text     "image_url"
+    t.text     "description"
   end
 
   create_table "reviews", force: :cascade do |t|

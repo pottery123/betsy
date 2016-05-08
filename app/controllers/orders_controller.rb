d31dba9e58e63aee2d71a6da0db1a7a227f6b888
Jade Vance <jadenicolevance@gmail.com>
2016-05-08 09:32:57 -0700

    outlined the order process to roll in confirmation of billing complete.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,52 +1,54 @@
 class OrdersController < ApplicationController
   # This should be the confirmation page for orders
- def show
-   @order = Order.find(session[:order_id])
- end
+  # after order is complete
+  def show
+    @order = Order.find(session[:order_id])
+  end
 
- def new
-   @order = Order.new
- end
+  def new
+    # this is where I think billing should be rolled into orders
+    # the page right after the cart
+    @order = Order.new
+  end
 
- def complete_purchase
-    locate the order 
+  def create
+    @order = Order.new(order_params[session[:order_id])
+    if @order.save
+      # call complete_order here 
+      # Send to the confirmation page aka orders#show
+    else
+      render :new
+    end
+  end
+
+  def complete_order
+    # locate the order 
     order = Order.find(session[:order_id])
-    # possible code to complete the order, needs testing:
 
-    # order.completed_time = Time.now
-    # order.completion_status = "complete"
+    # we need to add a status column to orders
+    
+    # order.updated_at = Time.now
+    # order.status = "complete"
+
     # reduce_inventory(order)
     # if order.save
-    #   redirect_to root_path
+    #   redirect_to confirmation_page_path
     # else
     #   render :new
     # end
   end
 
- def reduce_inventory(order)
-    @items = order.order_items
+  def reduce_inventory(order)
+    @items = OrderItem.find(order).quantity
+    @product = Product.find(order).name
     @items.each do |item|
-      product = item.product
       quantity = item.quantity
-      if product.stock < 0
+      if @product.quantity < 0
         flash[:error] = "Sorry, this is out of stock!"
         redirect_to product_path(params[:product_id])
       else
-        product.stock = product.stock - quantity
+        @product.quantity -= quantity
       end
-    end
-  end
-
- def find_order
-    @order = Order.find(session[:order_id])
- end
-
-  def create
-    @order = Order.new(order_params[session[:order_id])
-    if @order.save
-      redirect_to products_path
-    else
-      render :new
     end
   end
 end  

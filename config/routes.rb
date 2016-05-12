c678144bc323149d5f920b556bc188f426aafdf2
Rowan Cota <cota.rg@gmail.com>
2016-05-12 01:01:29 -0700

    working on routes
    commenting out routes that aren't being used for thorough testing before deletion. AFTER THIS IS TESTED, REFACTOR TO REMOVE COMMENTED OUT "DEPRECATED" ROUTES.

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,37 @@
 Rails.application.routes.draw do
 
   # Creates an empty cart and an order id for guests from session id
-    root 'sessions#create_order'
-
-  resources :products do
-    resources :reviews
-  end
-
- get '/complete' => 'orders#show', as: 'complete_order'
- 
- get '/checkout' => 'orders#new', as: 'checkout'
- post '/checkout' => 'orders#create'
- post '/checkout' => 'orders#complete_order'
+  root 'sessions#create_order'
+  get '/complete' => 'orders#show', as: 'complete_order'
+  get '/checkout' => 'orders#new', as: 'checkout'
+  
+  # THERE IS A DUPLICATE ROUTE HERE AND I DON'T KNOW WHICH ONE CHECKOUT NEEDS!
+  post '/checkout' => 'orders#create'
+  post '/checkout' => 'orders#complete_order'
 
   get '/cart' => 'order_item#index', as: 'cart'
   post '/cart' => 'order_item#create'
   delete '/cart' => 'order_item#destroy'
   patch '/cart/:id' => 'order_item#update', as: 'update_cart'
-
-
   get 'dashboard/:id' => 'users#show', :as => 'dashboard'
   # get 'dashboard/:id' => 'users#show_by_merchant', :as => 'dashboard'
-
   # get '/users/:user_id/products' => 'products#show_by_merchant', as: "user_products"
   # get '/account' => 'users'
-
   get  '/categories/:category_id/products' => 'products#show_by_category', as: "category_products"
-
+  delete '/logout' => 'sessions#destroy'
+  # get 'dashboard/:id' => 'users#show_by_merchant', :as => 'dashboard'
 
   resources :sessions, :only => [:new, :create, :destroy]
-  delete '/logout' => 'sessions#destroy'
 
-  #resources :billings
+  # resources :billings
 
   resources :users, :only => [:new, :create] do
     # resources :products (commented out to write what's needed by hand)
-    resources :orders
+    # resources :orders 
   end
 
   resources :products do
-    resources :reviews
+    resources :reviews, :only => [:new, :create]
     resources :orders
   end
 
@@ -48,15 +40,6 @@ Rails.application.routes.draw do
   resources :orders do
     resources :users
   end
-
-  # get 'dashboard/:id' => 'users#show_by_merchant', :as => 'dashboard'
-
-
-  resources :products do
-    resources :reviews
-    resources :orders
-  end
-
 
   # resources :reviews do
   #   resources :users

4259bf3ea5df3e9a4a9a81cad12dc10e8fd9aae8
Sarah <sarahkane682@yahoo.com>
2016-05-12 15:13:37 -0700

    working on validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ require 'date'
 
 class Order < ActiveRecord::Base
     has_many :order_items
-    validates_length_of :credit_card_number, is: 10,  message: "Number must be 16 digits long"
+    validates :credit_card_number, length: { is: 16 }
     # validates_length_of :zip, is: 5
     # validates_length_of :credit_card_number, is: 16
     # validates_length_of :rsecurity_on_cc, is: 3, message: "The three digit security number is located on the back of the card"

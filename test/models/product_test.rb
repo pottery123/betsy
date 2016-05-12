e8e75ca4e435ed12553a4c6bcf5a850ed3e969a8
Rowan Cota <cota.rg@gmail.com>
2016-05-11 23:44:23 -0700

    adding simplecov and writing tests.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,6 +9,37 @@ class ProductTest < ActiveSupport::TestCase
     product = Product.new
 
     assert_not product.valid?
+    assert product.errors.keys.include?(:name), "can't be empty"
+  end
+
+  test "products must have a whole number quantity" do
+    product = Product.new
+    product.name = "wedding football"
+    product.quantity = 0.3
+    product.price_in_dollars = 10
+
+    assert_not product.valid?
+    assert product.errors.keys.include?(:quantity), "is not a number"
+  end
+
+  test "products must have a quantity greater than 0" do
+    product = Product.new
+    product.name = "wedding football"
+    product.quantity = -3
+    product.price_in_dollars = 10
+
+    assert_not product.valid?
+    assert product.errors.keys.include?(:quantity), "is not a number"
+  end
+
+  test "products must have a price in dollars greater than 1" do
+    product = Product.new
+    product.name = "wedding football"
+    product.quantity = 3
+    product.price_in_dollars = 0.5
+
+    assert_not product.valid?
+    assert product.errors.keys.include?(:price_in_dollars), "is not a number"
   end
 
 end

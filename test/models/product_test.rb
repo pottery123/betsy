f1e5209d33865d0d6f14fc940be5ed1e9dd9af7b
Rowan Cota <cota.rg@gmail.com>
2016-05-11 22:03:37 -0700

    beginning work on tests for products and products controller.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,10 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # test "products must have names" do
+    
+  #   assert_not :nameless.valid?
+  # end
+
 end

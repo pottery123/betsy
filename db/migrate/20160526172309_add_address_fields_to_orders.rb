165171db4fec0166444bf55c0070f054b78660dd
Ania Gonzalez <aniagr@gmail.com>
2016-05-26 10:30:41 -0700

    Added city, state and country to orders

---
+++ db/migrate/20160526172309_add_address_fields_to_orders.rb
@@ -0,0 +1,7 @@
+class AddAddressFieldsToOrders < ActiveRecord::Migration
+  def change
+    add_column :orders, :city, :string
+    add_column :orders, :state, :string
+    add_column :orders, :country, :string
+  end
+end

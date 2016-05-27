338ba3de78b70451148163a0a80d2615a7138dc1
Ania Gonzalez <aniagr@gmail.com>
2016-05-27 14:27:58 -0700

    Changed URL base

--- app/models/ship_it_wrapper.rb
+++ app/models/ship_it_wrapper.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 module ShipItWrapper
 
-  BASE_URL = "http://localhost:3000"
+  BASE_URL = "http://ship-it-api.herokuapp.com"
 
   def self.get_quotes(address)
     HTTParty.get(BASE_URL + "/quotes", body: { shipping: {

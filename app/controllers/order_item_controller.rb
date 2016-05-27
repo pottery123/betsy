e8460e1130ef8e4b1b230d01e0191a0e4754035a
Adriana <dri19tcc@gmail.com>
2016-05-27 14:02:58 -0700

    Added more error handling

--- app/controllers/order_item_controller.rb
+++ app/controllers/order_item_controller.rb
@@ -17,12 +17,16 @@ class OrderItemController < ApplicationController
     #   flash[:nope] = "Something Went Wrong #{response.code}"
     # end
 
-    if !params[:zipcode].match(/^\d{5}(-\d{4})?$/) #&& params[:zipcode].to_i.class != Fixnum
+    if !params[:zipcode].match(/^\d{5}(-\d{4})?$/)
       flash[:nope] = "This needs to be a valid zipcode, ex: 98103"
     end
     if params[:zipcode]
-      quantity = @order_items.inject(0) { |sum, n| sum + n[:quantity]}
-      @quotes = BetsyShippingWrapper.get_quotes(params[:zipcode], quantity, @order.id)
+      if response.code == "200"
+        quantity = @order_items.inject(0) { |sum, n| sum + n[:quantity]}
+        @quotes = BetsyShippingWrapper.get_quotes(params[:zipcode], quantity, @order.id)
+      else
+        flash[:nope] = "Something Went Wrong #{response.code}"
+      end
     end
     render :index
   end
--- lib/betsy_shipping.rb
+++ lib/betsy_shipping.rb
@@ -4,7 +4,7 @@ module BetsyShippingWrapper
   API_URL = "http://localhost:3000/"
 
   def self.get_quotes(zipcode, quantity, order_id)
-    HTTParty.get(API_URL + "quotes?zipcode=#{zipcode}&quantity=#{quantity}&order_id=#{order_id}").parsed_response
+    response = HTTParty.get(API_URL + "quotes?zipcode=#{zipcode}&quantity=#{quantity}&order_id=#{order_id}")
+    return response
   end
-
 end

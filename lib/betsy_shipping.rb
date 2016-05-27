6eaa8f69621264784a824ee3494e702f3efff01f
Adriana <dri19tcc@gmail.com>
2016-05-27 15:09:00 -0700

    Added url for push to heroku

--- lib/betsy_shipping.rb
+++ lib/betsy_shipping.rb
@@ -1,7 +1,7 @@
 require 'httparty'
 
 module BetsyShippingWrapper
-  API_URL = "http://localhost:3000/"
+  API_URL = "https://quiet-thicket-75080.herokuapp.com/"
 
   def self.get_quotes(zipcode, quantity, order_id)
     response = HTTParty.get(API_URL + "quotes?zipcode=#{zipcode}&quantity=#{quantity}&order_id=#{order_id}")
